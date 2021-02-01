5c4c2641d47b1c125de5bc1464c07a84c482e3a6
catalina <cdianacatalina@gmail.com>
2021-02-01 17:30:44 +0100

    update fotos and titles - minor fixes

--- js-files/src.js
+++ js-files/src.js
@@ -1,17 +1,39 @@
-// turn the style :focus given by the css when the page is loaded
-document.getElementById("start-focus").focus()
+// gives the style :focus to the element.  Displays the tab programming
+// courses on the education section when the page is load.
+//todo: find another way!!!!!!!!!!!!
+//document.getElementById("start-focus").focus();
+// to take the foucs out of the education section and
+// give it to the top of the page, when the page is loaded.
+//document.querySelector("h1").focus();
 
-// The openEducation() function is called when the user clicks on one of the buttons of the tab menu.
-// it is called through the onclick at the button in html
-// it change on the content the style: block to display, none to not display, depending on which button has been pressed
-// buttons and content are connected with id of the content.
+/**
+ * @param {String} educationType;
+ * Control content displayed of the tab menu, which changes by clicking
+ * on a button of the tab.
+ */
 function openEducation(educationType) {
-  var selectedText = document.getElementById(educationType);
+  let selectedText = document.getElementById(educationType);
   // x is the array containing the content that each button is suppossed to display
-  var texts = document.getElementsByClassName("education");
-  
-  for (var i = 0; i < texts.length; i++) {
+  let texts = document.getElementsByClassName("education");
+
+  for (let i = 0; i < texts.length; i++) {
     // desactivates any text that is not being selected.
     texts[i].style.display = texts[i].id === selectedText.id ? "block" : "none";
   }
 }
+
+//----------------hover text for the icons--------------------------------
+// function displayText(e) {
+//   console.log("mouseover");
+//   let title = this.title;
+//   this.setAttribute("tooltip", title);
+// }
+
+// function hideText(e) {
+//   console.log("mouseout");
+//   this.removeAttribute("tooltip");
+// }
+
+// let jsIcon = document.querySelector("#js");
+// jsIcon.addEventListener("mouseover", displayText, false);
+// jsIcon.addEventListener("mouseout", hideText, false);
